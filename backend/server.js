234202c73c1630faea271fce7be1a48e577c2e72
Adarsh Gayadolli <137860068+Adarshpg@users.noreply.github.com>
2025-07-02 16:14:24 +0530

    dlt extra

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,9 @@ const server = http.createServer(app);
 let allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5000',
-  'https://registration-page-7c8o.vercel.app',
   'https://adarshpg-registration-page.onrender.com',
   'https://register.medinitechnologies.in',
+  'https://registration-page-q8cs.vercel.app',s
 ];
 
 // Add any additional origins from environment variable
